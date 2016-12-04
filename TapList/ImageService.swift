983787c580662e5c161a8b3663d3230bbf88013b
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-04 13:40:19 -0500

    Create method for fetching images.

--- TapList/ImageService.swift
+++ TapList/ImageService.swift
@@ -6,10 +6,42 @@
 //  Copyright © 2016 Anthony Whitaker. All rights reserved.
 //
 
+import Alamofire
 import Foundation
+import UIKit
 
-let baseUrl = "https://www.kroger.com/product/images/medium/front/"
-
-func image(for product: Product) {
+class ImageService {
     
+    let instance = ImageService()
+    
+    let baseUrl = "https://www.kroger.com/product/images/medium/front/"
+    
+    let imageCache = NSCache<NSString, UIImage>()
+    
+    private init(){}
+    
+    func image(for product: Product, completion: @escaping (UIImage?) -> ()) -> DataRequest? {
+        guard let id = product.id else {
+            completion(nil)
+            return nil
+        }
+        
+        var request: DataRequest? = nil
+        
+        if let image = imageCache.object(forKey: id as NSString) {
+            completion(image)
+        } else {
+            let url = "\(baseUrl)\(id)"
+            request = Alamofire.request(url).validate(contentType: ["image/*"]).responseData(completionHandler: { responseData in
+                if let data = responseData.data {
+                    if let image = UIImage(data: data) {
+                        self.imageCache.setObject(image, forKey: url as NSString)
+                        completion(image)
+                    }
+                }
+            })
+        }
+        
+        return request
+    }
 }
