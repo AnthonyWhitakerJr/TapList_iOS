bd8117e4c6ab9ffa2c95ec588ea044890c0633c7
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-04 14:36:42 -0500

    Set image in Product...Cell when configured.

--- TapList/ProductCollectionViewCell.swift
+++ TapList/ProductCollectionViewCell.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Alamofire
 
+@IBDesignable
 class ProductCollectionViewCell: UICollectionViewCell {
 
+    var product: Product!
+    var imageRequest: DataRequest?
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configureCell(product: Product) {
+        self.product = product
+        setImage()
+    }
+    
+    private func setImage() {
+        if let imageRequest = imageRequest {
+            imageRequest.cancel()
+        }
+        
+        self.productImageView.image = nil // TODO: Replace with placeholder image.
+        imageRequest = ImageService.instance.image(for: product, completion: {image in
+            if let image = image {
+                self.productImageView.image = image
+            }
+        })
     }
 
 }
