d7947bff4af6f5fede6a61939fbc56f2726b8215
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-07 15:50:13 -0500

    Trim to functional code.

--- TapList/TestViewController.swift
+++ TapList/TestViewController.swift
@@ -10,51 +10,17 @@ import UIKit
 
 class TestViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        registerForPreviewing(with: self, sourceView: view)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("------------------------------------------------------")
         if segue.identifier == "popover1" || segue.identifier == "popover2" {
             let popoverViewController = segue.destination as! SalePriceViewController
             popoverViewController.modalPresentationStyle = .popover
             popoverViewController.popoverPresentationController!.delegate = self
             
-            if let sender = sender as? UIButton {
-                print("Button!")
-                print("sourceView: \(popoverViewController.popoverPresentationController?.sourceView)")
-                print("sender: \(sender)")
-                print(popoverViewController.popoverPresentationController?.sourceView == sender)
-                print("sourceRect: \(popoverViewController.popoverPresentationController?.sourceRect)")
-                print("sender.frame: \(sender.frame)")
-                print(popoverViewController.popoverPresentationController?.sourceRect == sender.frame)
-                
-
-            }
-            
             // Set bounds for arrow placement.
             if let sender = sender as? UIButton {
                 popoverViewController.popoverPresentationController?.sourceRect = CGRect(x: 0, y: 0, width: sender.frame.width, height: sender.frame.height)
-            }
-
-            
-            if(popoverViewController.popoverPresentationController?.sourceView == nil) {
-                popoverViewController.popoverPresentationController?.sourceView = self.view
-                print("Overlap?: \(popoverViewController.popoverPresentationController?.canOverlapSourceViewRect)")
-                popoverViewController.popoverPresentationController?.canOverlapSourceViewRect = true
-                print("SET")
             }
             
             switch segue.identifier! {
@@ -67,7 +33,6 @@ class TestViewController: UIViewController {
             default: break
             }
         }
-        
     }
 
 }
