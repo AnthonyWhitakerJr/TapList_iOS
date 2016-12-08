0962f219a5c758a0f8a10ba8ef272d95635252e4
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-08 17:17:53 -0500

    Clean out test controller.

--- TapList/TestViewController.swift
+++ TapList/TestViewController.swift
@@ -10,35 +10,4 @@ import UIKit
 
 class TestViewController: UIViewController {
 
-    // MARK: - Navigation
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "popover1" || segue.identifier == "popover2" {
-            let popoverViewController = segue.destination as! SalePriceViewController
-            popoverViewController.popoverPresentationController?.delegate = self
-            
-            // Set bounds for arrow placement.
-            if let sender = sender as? UIButton {
-                popoverViewController.popoverPresentationController?.sourceRect = CGRect(x: 0, y: 0, width: sender.frame.width, height: sender.frame.height)
-            }
-            
-            switch segue.identifier! {
-            case "popover1":
-                let product = Product(upc: "0001111016222", name: "Kroger Glazed Sour Cream Cake Donut Holes", listPrice: 2.49, offerPrice: 1.50, detail: "14 oz")
-                popoverViewController.product = product
-            case "popover2":
-                let product = Product(upc: "0001111041600", name: "Kroger 2% Reduced Fat Milk", listPrice: 1.69, offerPrice: 1.49, detail: "1/2 gal")
-                popoverViewController.product = product
-            default: break
-            }
-        }
-    }
-
-}
-
-extension TestViewController: UIPopoverPresentationControllerDelegate {
- 
-    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-        return .none
-    }
 }
