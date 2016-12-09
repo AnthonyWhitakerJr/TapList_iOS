030030e808e129b4f5e9108f0a0cab52dc570ca4
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-08 20:02:56 -0500

    Move Offers to database.

--- TapList/DataService.swift
+++ TapList/DataService.swift
@@ -14,10 +14,25 @@ class DataService {
     static let instance = DataService()
     
     let database: FIRDatabaseReference
+    let offer: FIRDatabaseReference
     let product: FIRDatabaseReference
     
     private init(){
         database = FIRDatabase.database().reference()
+        offer = database.child("offer")
         product = database.child("product")
+    }
+    
+    func offer(for productSku: String, completion: @escaping (_: Offer?) -> ()) {
+        let offerRef = offer.child(productSku)
+        
+        offerRef.observeSingleEvent(of: .value , with: { snapshot in
+            if let offerData = snapshot.value as? Dictionary<String, Any> {
+                let offer = Offer(productSku: productSku, data: offerData)
+                completion(offer)
+            } else {
+                completion(nil)
+            }
+        })
     }
 }
