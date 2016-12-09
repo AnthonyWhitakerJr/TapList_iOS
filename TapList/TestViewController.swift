1d9f510886f98d455192b288551522b009f58675
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-08 23:45:30 -0500

    Testing placeholder text

--- TapList/TestViewController.swift
+++ TapList/TestViewController.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
-class TestViewController: UIViewController {
+class TestViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    var placeholderLabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.delegate = self
+        placeholderLabel = UILabel()
+        placeholderLabel.text = "Enter some text..."
+        placeholderLabel.font = UIFont.italicSystemFont(ofSize: (textView.font?.pointSize)!)
+        placeholderLabel.sizeToFit()
+        textView.addSubview(placeholderLabel)
+        placeholderLabel.frame.origin = CGPoint(x: 5, y: (textView.font?.pointSize)! / 2)
+        placeholderLabel.textColor = .gray //UIColor(white: 0, alpha: 0.3)
+        placeholderLabel.isHidden = !textView.text.isEmpty
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        placeholderLabel.isHidden = !textView.text.isEmpty
+    }
 }
