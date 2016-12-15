e69ae713b6995d8a238ec2c77aabff7844e9fca5
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-14 21:37:55 -0500

    Add scrolling when keyboard appears.

--- TapList/TestViewController.swift
+++ TapList/TestViewController.swift
@@ -12,14 +12,38 @@ class TestViewController: UIViewController, QuantityViewDataSource {
 
     @IBOutlet weak var quantityButton: UIButton!
     @IBOutlet weak var quantityTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureDismissKeyboardOnTap()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+        
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         configureQuantityView(previousQuantity: 4)
+    }
+    
+    func keyboardWillShow(notification:NSNotification){
+        //give room at the bottom of the scroll view, so it doesn't cover up anything the user needs to tap
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        self.scrollView.contentInset = contentInset
+    }
+    
+    func keyboardWillHide(notification:NSNotification){
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        self.scrollView.contentInset = contentInset
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
