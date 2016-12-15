4b8a6505fa2e1a17142141e9b363738b106a3447
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-15 17:54:33 -0500

    Strip contents from test view controller.

--- TapList/TestViewController.swift
+++ TapList/TestViewController.swift
@@ -8,55 +8,7 @@
 
 import UIKit
 
-class TestViewController: UIViewController, QuantityViewDataSource {
+class TestViewController: UIViewController {
 
-    @IBOutlet weak var quantityButton: UIButton!
-    @IBOutlet weak var quantityTextField: QuantityTextField!
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-    var keyboardHandler: KeyboardHandler!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        keyboardHandler = KeyboardHandler(contextView: self.view, scrollView: scrollView, onlyScrollForKeyboard: true)
-        keyboardHandler.startDismissingKeyboardOnTap()
-        keyboardHandler.startObservingKeyboardEvents()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        configureQuantityView(previousQuantity: 4)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "quantityPopover" {
-            guard let controller = segue.destination as? QuantityTableViewController else {
-                print("improper controller for this segue")
-                return
-            }
-            
-            preparePopover(for: controller, sender: sender)
-            
-            controller.delegate = self
-            controller.previousQuantity = quantityButton.currentTitle
-        }
-    }
-    
-    func preparePopover(for controller: UIViewController, sender: Any?) {
-        controller.popoverPresentationController?.delegate = self
-        
-        // Set bounds for arrow placement.
-        if let sender = sender as? UIButton {
-            controller.popoverPresentationController?.sourceView = sender
-            controller.popoverPresentationController?.sourceRect = CGRect(x: 0, y: 0, width: sender.frame.width, height: sender.frame.height)
-        }
-        
-        controller.modalPresentationStyle = .popover
-    }
 }
 
-extension TestViewController: UIPopoverPresentationControllerDelegate {
-    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-        return .none
-    }
-}
