2faffe1c6c2744beb26e8fb11ae2eeb40f1518d4
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-16 14:22:42 -0500

    Cart Management View & Controller.

---
+++ TapList/CartItemTableViewCell.swift
@@ -0,0 +1,93 @@
+//
+//  CartItemTableViewCell.swift
+//  TapList
+//
+//  Created by Anthony Whitaker on 12/16/16.
+//  Copyright © 2016 Anthony Whitaker. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CartItemTableViewCell: UITableViewCell, QuantityView {
+
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var quantityButton: UIButton!
+    @IBOutlet weak var quantityTextField: QuantityTextField!
+    @IBOutlet weak var offerPriceButton: UIButton!
+    
+    var delegate: ProductCellDelegate?
+    var imageRequest: DataRequest?
+    var cartItem: CartItem!
+    var product: Product!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 1
+    }
+    
+    func configureCell(cartItem: CartItem) {
+        self.cartItem = cartItem
+        
+        configureQuantityView(previousQuantity: cartItem.quantity)
+        
+        DataService.instance.product(for: cartItem.sku) { product in
+            if let product = product {
+                self.product = product
+                
+                self.setImage()
+                
+                self.productNameLabel.text = product.name
+                
+                let formatter = NumberFormatter()
+                formatter.numberStyle = .currency
+                
+                if let offerPrice = product.offerPrice {
+                    self.priceLabel.isHidden = true
+                    self.offerPriceButton.isHidden = false
+                    
+                    let totalPrice = offerPrice * Double(cartItem.quantity)
+                    self.offerPriceButton.setTitle(formatter.string(from: NSNumber(value: totalPrice))!, for: .normal)
+                } else {
+                    self.priceLabel.isHidden = false
+                    self.offerPriceButton.isHidden = true
+                    
+                    let totalPrice = product.listPrice! * Double(cartItem.quantity)
+                    self.priceLabel.text = formatter.string(from: NSNumber(value: totalPrice))
+                }
+                
+                self.detailLabel.text = product.detail
+            }
+        }
+    }
+    
+    private func setImage() {
+        if let imageRequest = imageRequest {
+            imageRequest.cancel() // Cancel any ongoing image requests (can happen when user scrolls quickly).
+        }
+        
+        self.productImageView.image = nil // TODO: Replace with placeholder image.
+        imageRequest = ImageService.instance.image(for: product, completion: {image in
+            self.productImageView.image = image
+        })
+    }
+    
+    @IBAction func offerButtonTapped(_ sender: UIButton) {
+        guard let product = product else {
+            return
+        }
+        
+        delegate?.handleOfferButtonTapped(product: product, sender: sender)
+    }
+    
+    @IBAction func productImageButtonPressed(_ sender: UIButton) {
+        guard let product = product else {
+            return
+        }
+        
+        delegate?.handleProductImageButtonTapped(product: product, sender: sender)
+    }
+}
---
+++ TapList/ManageCartTableViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ManageCartTableViewController.swift
+//  TapList
+//
+//  Created by Anthony Whitaker on 12/16/16.
+//  Copyright © 2016 Anthony Whitaker. All rights reserved.
+//
+
+import UIKit
+
+class ManageCartTableViewController: UITableViewController {
+
+    var cart: Cart {
+        return DataService.instance.cart
+    }
+    
+    var cartItems: Array<CartItem> {
+        return Array(DataService.instance.cart.cartItems.values)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cartItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath) as? CartItemTableViewCell {
+            let cartItem = cartItems[indexPath.row]
+            cell.configureCell(cartItem: cartItem)
+
+            return cell
+        }
+        return UITableViewCell()
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
