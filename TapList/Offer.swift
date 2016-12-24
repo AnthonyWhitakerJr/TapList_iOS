faec9eb4d6c3c4d2aaf35aaa25c3867b0d915560
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-24 14:23:45 -0500

    Hard-code string divider and currency from Offer. Can replace if/when there is ever a need. Until then it is just noise.

--- TapList/Offer.swift
+++ TapList/Offer.swift
@@ -28,21 +28,16 @@ class Offer {
     var endDate: String? // TODO: Change to Date.
     var offerType: OfferType?
     var offerQuantity: Int?
-    var stringDivider: String? // Is this needed? Hard-code?
-    var currency: String? // Should this be determined by localization?
     var offerPriceTitle: String?
     var offerDescription: String?
     var offerShortDescription: String?
     
     init(productSku: String, endDate: String?, offerType: OfferType?, offerQuantity: Int?,
-         stringDivider: String? = "/", currency: String? = "$",
          offerPriceTitle: String?, offerDescription: String?, offerShortDescription: String?) {
         self.productSku = productSku
         self.endDate = endDate
         self.offerType = offerType
         self.offerQuantity = offerQuantity
-        self.stringDivider = stringDivider
-        self.currency = currency
         self.offerPriceTitle = offerPriceTitle
         self.offerDescription = offerDescription
         self.offerShortDescription = offerShortDescription
--- TapListTests/OfferTests.swift
+++ TapListTests/OfferTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import TapList
 
 class OfferTests: XCTestCase {
     
