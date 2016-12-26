c7db447c9ff929cdb88a31dc2463a0340f886283
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-26 16:42:19 -0500

    Document RoundedButton

--- TapList/RoundedButton.swift
+++ TapList/RoundedButton.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+/// A `UIButton` that allows corner radius to be defined and viewed in Interface Builder.
 @IBDesignable
 class RoundedButton: UIButton {
 
+    /// The radius to use when drawing rounded corners for the layer’s background.
+    ///
+    /// Setting the radius to a value greater than 0.0 causes the layer to begin drawing rounded corners on its background
+    /// and content to be clipped to the rounded corners.
+    ///
+    /// The default value of this property is 0.0.
     @IBInspectable var cornerRadius: CGFloat {
         get {
             return layer.cornerRadius
