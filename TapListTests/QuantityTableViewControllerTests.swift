e13e616685befb6be29fbbbf3749c9b27b9efeeb
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-12-29 12:03:35 -0500

    Test QuantityTableViewController.

--- TapListTests/QuantityTableViewControllerTests.swift
+++ TapListTests/QuantityTableViewControllerTests.swift
@@ -29,4 +29,58 @@ class QuantityTableViewControllerTests: XCTestCase {
         XCTAssertNotNil(controller)
     }
     
+    func testCellForRow() {
+        controller.previousQuantity = "4"
+        for i in 0...9 {
+            let indexPath = IndexPath(row: i, section: 0)
+            let cell = controller.tableView(controller.tableView, cellForRowAt: indexPath) as? QuantityTableViewCell
+            
+            switch i {
+            case 9:
+                XCTAssertEqual("10+", cell?.quantityLabel.text)
+            case 3:
+                XCTAssertEqual("4", cell?.quantityLabel.text)
+                XCTAssertEqual(UIColor.lightGray.withAlphaComponent(0.5), cell?.backgroundColor)
+            default:
+                XCTAssertEqual("\(i + 1)", cell?.quantityLabel.text)
+            }
+        }
+    }
+    
+    func testCellForRow_SanityCheck() {
+        let controller = QuantityTableViewController()
+        let indexPath = IndexPath(row: 0, section: 0)
+        controller.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "quantityCell")
+        let cell = controller.tableView(controller.tableView, cellForRowAt: indexPath)
+        
+        XCTAssertNotNil(cell)
+        XCTAssertFalse(cell is QuantityTableViewCell)
+    }
+    
+    func testRowSelection() {
+        class TestDelegate: QuantityTableViewControllerDelegate {
+            var quantity: String?
+            func update(selectedQuantity: String) {
+                quantity = selectedQuantity
+            }
+        }
+        
+        let delegate = TestDelegate()
+        controller.delegate = delegate
+        
+        XCTAssertEqual(10, controller.tableView.numberOfRows(inSection: 0)) // Initialize table.
+        
+        let indexPath = IndexPath(row: 6, section: 0)
+        controller.tableView(controller.tableView, didSelectRowAt: indexPath)
+        
+        XCTAssertEqual("7", delegate.quantity)
+    }
+    
+    func testSize() {
+        controller.viewWillAppear(false)
+        let expectedSize = CGSize(width: 375, height: 440)
+        
+        XCTAssertEqual(expectedSize, controller.view.frame.size)
+    }
+    
 }
